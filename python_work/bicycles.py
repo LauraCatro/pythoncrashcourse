897d242fe9b48128036a3385308590668e65def8
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-11-26 14:26:55 -0600

    accessing_elements_in_list

---
+++ python_work/bicycles.py
@@ -0,0 +1,13 @@
+bicycles = ['trek','cannondale','redline', 'specialized']
+message = f"My firts bicycle was a {bicycles[0].title()}"
+
+print(bicycles[0])
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+
+print(message)
+
+
