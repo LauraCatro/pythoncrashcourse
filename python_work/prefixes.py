5553196d8a2b433115253f89fbdd34dccb57a045
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-11-26 11:16:45 -0600

    prefixes

---
+++ python_work/prefixes.py
@@ -0,0 +1,8 @@
+nostarch_url = "https://nostarch.com"
+print(nostarch_url.removeprefix("https://"))
+
+simple_url = nostarch_url.removeprefix("https://")
+print(simple_url)
+
+full_name = "Diana Laura Castro López"
+print(full_name.removeprefix("Diana Laura "))
