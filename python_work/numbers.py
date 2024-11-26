eaea6f7afa04ddc22f31611b980639bcf90614ec
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-11-26 12:42:59 -0600

    numbers in py

---
+++ python_work/numbers.py
@@ -0,0 +1,21 @@
+#Diana Laura Castro López
+#11/26/2024
+
+#Multiple Assignment
+x,y,z = 1,2,3
+
+print(x+y)
+
+#Ejercicio 1
+favorite_number = 19
+message = f"My favorite number is {favorite_number}"
+print(message)
+
+#Enercicio 2
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
+
+
+
