d633b319510026407d2c63e4bc5849409b3d7d99
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-11-29 08:29:00 -0600

    Working with while

--- python_work/movie_tickets.py
+++ python_work/movie_tickets.py
@@ -1,15 +1,15 @@
 prompt = "\nPor favor ingresa tu edad para indicar el costo del boleto: "
-prompt += "Enter 0 for exit"
+prompt += "Enter -1 for exit"
 
-# while True:
-#    x = input(prompt)
-#    age = int(x)
-#
-#    if age <= 3:
-#        print("The ticket is free")
-#    elif age > 3 and age <= 12:
-#        print("The ticket is $10")
-#    elif age > 12:
-#        print("The ticket is $15")
-#    elif age == 0:
-#        break
+while True:
+    x = input(prompt)
+    age = int(x)
+
+    if age > 0 and age < 3:
+        print("The ticket is free")
+    elif age > 3 and age <= 12:
+        print("The ticket is $10")
+    elif age > 12:
+        print("The ticket is $15")
+    elif age < 0:
+        break
--- python_work/sandwichs.py
+++ python_work/sandwichs.py
@@ -1,13 +1,18 @@
-sandwich_orders = ['jamon', 'frijoles', 'huevo']
+sandwich_orders = ['Ham', 'Beans', 'Egg', 'pastrami', 'pastrami', 'pastrami']
 finished_sandwich = []
+
+print("We no longer have pastrami sandwiches")
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
 
 for sandwich in sandwich_orders:
     print(f"I made your {sandwich.lower()} sandwich")
 
 while sandwich_orders:
     sandwich = sandwich_orders.pop()
-
     finished_sandwich.append(sandwich)
 
 for sandwich in finished_sandwich:
-    print(f"This sandwich de {sandwich} esta hecho")
+    print(f"This {sandwich} sandwich is made")
