2c90696e13923d1620ea31cf6c7b09c289a42d8e
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-04 11:51:14 -0600

    Working with classes

---
+++ python_work/classes_restaurant_icecream.py
@@ -0,0 +1,41 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        print(f"The name of the restaurant is: {self.restaurant_name} ")
+        print(f"The type of cuisine is: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"The restaurant '{self.restaurant_name}' is open!")
+
+    def read_number_served(self):
+        print(f"Number Served: {self.number_served}")
+
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, number):
+        self.number_served += number
+
+
+class IceCreamStand(Restaurant):
+
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['Strawberry', 'Lemon', 'Chocolate']
+
+    def decribe_flavors(self):
+        print("Ice cream flavors: ")
+        for flavor in self.flavors:
+            print(f"- {flavor}")
+
+# flavors = ['fresa','chocolate','limon']
+
+
+ice = IceCreamStand('La michoacana', 'Ice cream')
+ice.describe_restaurant()
+ice.decribe_flavors()
