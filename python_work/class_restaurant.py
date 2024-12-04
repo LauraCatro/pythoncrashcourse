8dde41697b42b3111888adb3bee71d28f9bd42e6
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-04 08:45:28 -0600

    Working with classes

--- python_work/class_restaurant.py
+++ python_work/class_restaurant.py
@@ -3,20 +3,33 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
-        print(f"\nThe name of the restaurant is: {self.restaurant_name} ")
+        print(f"The name of the restaurant is: {self.restaurant_name} ")
         print(f"The type of cuisine is: {self.cuisine_type}")
-    
+
     def open_restaurant(self):
         print(f"The restaurant '{self.restaurant_name}' is open!")
 
+    def read_number_served(self):
+        print(f"Number Served: {self.number_served}")
 
-restaurant = Restaurant('The Green Fork','Vegetarian Cuisine')
-restaurant_1 = Restaurant('Spice Haven', 'Indian Cuisine')
-restaurant_2 = Restaurant('Bella Pasta','Italian Cuisine')
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, number):
+        self.number_served += number
+
+
+restaurant = Restaurant('The Green Fork', 'Vegetarian Cuisine')
+restaurant.set_number_served(100)
+restaurant.read_number_served()
+restaurant.increment_number_served(300)
+restaurant.read_number_served()
+# restaurant_1 = Restaurant('Spice Haven', 'Indian Cuisine')
+# restaurant_2 = Restaurant('Bella Pasta','Italian Cuisine')
 
 restaurant.describe_restaurant()
-restaurant_1.describe_restaurant()
-restaurant_2.describe_restaurant()
-        
+# restaurant_1.describe_restaurant()
+# restaurant_2.describe_restaurant()
--- python_work/classes_users.py
+++ python_work/classes_users.py
@@ -5,27 +5,49 @@ class User:
         self.last = last_name
         self.age = age
         self.nacionality = nacionality
-    
+        self.login_attempts = 1
+
     def describe_user(self):
         print("\nUser Information: ")
         print(f"First Name: {self.first}")
         print(f"Last Name: {self.last}")
         print(f"Age: {self.age}")
         print(f"Nacionality: {self.nacionality}")
-    
+
     def greet_user(self):
         print(f"\nHello {self.first} {self.last}")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+    def read_number_login_attempts(self):
+        print(f"Number users: {self.login_attempts}")
+
 
 user = User('Diana', 'Laura', 20, 'Mexicana')
-user_1 = User('Angeles', 'Marquez', 69, 'Mexicana')
-user_2 = User('Antonio', 'Herrera', 24, 'Mexicano')
+# user_1 = User('Angeles', 'Marquez', 69, 'Mexicana')
+# user_2 = User('Antonio', 'Herrera', 24, 'Mexicano')
 
 user.describe_user()
-user.greet_user()
+print(f"{user.login_attempts}")
 
-user_1.describe_user()
-user_1.greet_user()
+# Increment login attempts
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.read_number_login_attempts()
 
-user_2.describe_user()
-user_2.greet_user()
+# Reset Login attempts
+user.reset_login_attempts()
+user.read_number_login_attempts()
+
+# user_1.describe_user()
+# user_1.greet_user()
+
+# user_2.describe_user()
+# user_2.greet_user()
