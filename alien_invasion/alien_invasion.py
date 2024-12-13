948210feaa8caed5bb07ef12ec018ded9e8bea46
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-13 09:02:45 -0600

    Setting the Background Color

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,18 +1,23 @@
 import sys
 import pygame
 
+
 class AlienInvasion:
     """
     Overall class to manage game assets and behavior.
     """
+
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-        
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -20,7 +25,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
