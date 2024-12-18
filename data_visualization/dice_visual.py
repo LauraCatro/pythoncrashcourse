1e99c0dbee82450ece6d48c3da67f3c0696f0343
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-18 16:31:31 -0600

    Analyzing the Results

--- data_visualization/dice_visual.py
+++ data_visualization/dice_visual.py
@@ -5,8 +5,15 @@ dice = Dice()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll in range(100):
+for roll in range(1000):
     result = dice.roll()
     results.append(result)
 
-print(results)
+# Analyze the results.
+frequencies = []
+poss_results = range(1, dice.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
