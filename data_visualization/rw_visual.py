b0375e39759530ffe33cd266e50ee57a74fc3555
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-18 12:41:43 -0600

    Generating Multiple Random Walks

--- data_visualization/rw_visual.py
+++ data_visualization/rw_visual.py
@@ -3,12 +3,20 @@ from random_walk import RandomWalk
 #from matplotlib import colormaps
 #print(colormaps)
 
-rw = RandomWalk()
-rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap=plt.cm.Spectral, s=15)
-ax.set_aspect('equal')
-plt.show()
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap=plt.cm.Spectral, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input("Make another walk? [y/n]: ")
+    if keep_running == 'n':
+        break
