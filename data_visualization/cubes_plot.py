7211b98dd4e2cf1dc7bfa897f2bf896eda41ca9c
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-18 11:05:13 -0600

    Cubes

---
+++ data_visualization/cubes_plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1,6)
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+# Define Custom Colors
+# ax.scatter(x_values, y_values,color='purple', s=10)
+
+# Using a Colormap
+ax.scatter(x_values, y_values,c=y_values, cmap=plt.cm.Blues, s=100)
+
+
+# Set chart title label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+ax.axis([0, 6, 0, 130])
+ax.ticklabel_format(style='plain')
+
+# Set size of tick labels.
+ax.tick_params(labelsize=14)
+
+plt.show()
+
+
