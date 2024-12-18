856481d7b25b638aa43cc6c1386d2f86a4b3a256
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-18 17:15:21 -0600

    Further Customizations

--- data_visualization/die_visual.py
+++ data_visualization/die_visual.py
@@ -23,4 +23,8 @@ for value in poss_results:
 title = "Results of Rolling Two D6 1,000 Times"
 labels = {'x':'Results', 'y':'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
 fig.show()
