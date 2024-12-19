2219797fa74c6f27c98dc577b62a10e2ab1d0cc6
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-19 17:57:43 -0600

    Try it Yourself 'Dice Comprehensions'

--- data_visualization/three_dice.py
+++ data_visualization/three_dice.py
@@ -1,4 +1,6 @@
 import plotly.express as px
+
+from data_visualization.die_visual import dice_1
 from dice import Dice
 
 # Create a D6 and a D10
@@ -7,18 +9,18 @@ dice_2 = Dice(6)
 dice_3 = Dice(6)
 
 # Make some rolls, and store results in a list.
-results = []
-for roll in range(1_000):
-    result = dice_1.roll() + dice_2.roll() + dice_3.roll()
-    results.append(result)
+# results = []
+#for roll in range(1_000):
+#    result = dice_1.roll() + dice_2.roll() + dice_3.roll()
+#    results.append(result)
+
+# List Comprehensions
+results = [dice_1.roll() + dice_2.roll() + dice_3.roll() for roll in range(1_000)]
 
 # Analyze the results.
-frequencies = []
 max_results = dice_1.num_sides + dice_2.num_sides + dice_3.num_sides
 poss_results = range(3, max_results+1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in poss_results]
 
 # Visualize the results.
 title = "Results of Rolling a Three D6 1,000 Times"
