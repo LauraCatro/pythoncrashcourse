f27ed4a6c12d4a679fe7e6026ef4bcabe8f4e68b
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-19 17:44:22 -0600

    Try it yourself 'Three Dice'

---
+++ data_visualization/three_dice.py
@@ -0,0 +1,32 @@
+import plotly.express as px
+from dice import Dice
+
+# Create a D6 and a D10
+dice_1 = Dice(6)
+dice_2 = Dice(6)
+dice_3 = Dice(6)
+
+# Make some rolls, and store results in a list.
+results = []
+for roll in range(1_000):
+    result = dice_1.roll() + dice_2.roll() + dice_3.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_results = dice_1.num_sides + dice_2.num_sides + dice_3.num_sides
+poss_results = range(3, max_results+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = "Results of Rolling a Three D6 1,000 Times"
+labels = {'x':'Results', 'y':'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
+# fig.write_html('dice_visual_d6d10.html')
+fig.show()
