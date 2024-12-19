5a82884586fb229d333505fa47a977f9c09e6773
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-19 14:53:32 -0600

    Extracting Magnitudes

--- data_visualization/eq_explore_data.py
+++ data_visualization/eq_explore_data.py
@@ -8,7 +8,13 @@ all_eq_data = json.loads(contents)
 
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
 
 # Create a more readable version of the data file.
 path = Path('eq_data/readable_eq_data.geojson')
