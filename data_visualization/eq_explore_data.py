e35d8f737328ba7c59bbcf1979c8058ebdf93029
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-19 15:13:31 -0600

    Building a World Map

--- data_visualization/eq_explore_data.py
+++ data_visualization/eq_explore_data.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+
 # Read data as a string and convert to a Python object.
 path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
@@ -18,9 +20,13 @@ for eq_dict in all_eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
+titile = 'Global Earthquakes'
+fig = px.scatter_geo(lat=lats, lon=lons, title=titile)
+fig.show()
+
+# print(mags[:10])
+# print(lons[:5])
+# print(lats[:5])
 # Create a more readable version of the data file.
 path = Path('eq_data/readable_eq_data.geojson')
 readable_contents = json.dumps(all_eq_data, indent=4)
